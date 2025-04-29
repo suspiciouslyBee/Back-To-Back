88fa4caf0afd66fa18ca98fb7b5c3472cc8d614a
suspiciouslyBee <suspiciously@bee.movie>
2025-04-28 18:00:32 -0700

    MainManager script now makes singleton

--- Assets/Scripts/Managers/MainManager.cs
+++ Assets/Scripts/Managers/MainManager.cs
@@ -1,10 +1,25 @@
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
+    //Makes into *global* singleton
+    public static MainManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {   
         
     }
 
