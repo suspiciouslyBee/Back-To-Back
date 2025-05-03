c85568c3bdedb81f91de760205e1bb2eda23a8cd
Spudoku <calebisntshort@gmail.com>
2025-05-02 21:27:54 -0700

    Built a simple game over screen fleshed out UIManager some more
    30 minutes

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 /* 
     UIManager
     Description:
@@ -11,8 +13,11 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     private static UIManager UIMInstance;
+    [SerializeField] private UIDocument gameOverDoc;
+    [SerializeField] private UIDocument pauseMenuDoc;
     public static UIManager Instance { get { return UIMInstance; } }
 
+    private bool paused;
     private void Awake()
     {
         if (UIMInstance != null && UIMInstance != this)
@@ -23,6 +28,73 @@ public class UIManager : MonoBehaviour
         {
             UIMInstance = this;
         }
+        InitAll();
+        paused = false;
     }
+
+
+
+
+    // animate the GameOver screen
+    public void GameOverSequence()
+    {
+
+    }
+
+    public void TogglePause()
+    {
+        paused = !paused;
+
+        // hide/show pause menu
+        // if (paused)
+        // {
+        //     gameOverUI.style.opacity = 100f;
+        // }
+        // else
+        // {
+        //     gameOverUI.style.opacity = 0;
+        // }
+
+    }
+
+    // prepares all variables
+    private void InitAll()
+    {
+        InitGameOver();
+        InitPause();
+    }
+
+    // handles GameOver document initialization
+    private void InitGameOver()
+    {
+        if (gameOverDoc != null)
+        {
+            gameOverUI = gameOverDoc.rootVisualElement.Q<VisualElement>("MainContainer");
+        }
+        else
+        {
+            throw new NullReferenceException("No Game Over UI Document found in scene!");
+        }
+
+
+    }
+
+    private void InitPause()
+    {
+        if (pauseMenuDoc != null)
+        {
+            pauseMenuUI = gameOverDoc.rootVisualElement.Q<VisualElement>("MainContainer");
+        }
+        else
+        {
+            throw new NullReferenceException("No Pause Over UI Document found in scene!");
+        }
+
+
+    }
+
+    // private UI elements
+    private VisualElement gameOverUI;
+    private VisualElement pauseMenuUI;
 
 }
