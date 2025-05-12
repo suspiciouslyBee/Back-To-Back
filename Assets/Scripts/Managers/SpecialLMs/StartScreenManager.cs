f71186df54d600403b223e48d100f801c6be3b23
suspiciouslyBee <suspiciously@bee.movie>
2025-05-11 17:18:06 -0700

    * so, apparently overloading works funny in c#. ive had to refactor the level manager a bit to get this working. protected virtual to let a derived class make protected override + stub start ui manager

--- Assets/Scripts/Managers/SpecialLMs/StartScreenManager.cs
+++ Assets/Scripts/Managers/SpecialLMs/StartScreenManager.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StartScreenManager : LevelManager
 {
+
+    [SerializeField]
+    private UIDocument mainMenu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void Start()
     {
-        
+        //need to show main menu
+
+
+    }
+
+    protected override void FixedUpdate()
+    {
+        //should do nothing?
     }
 
     // Update is called once per frame
@@ -13,4 +24,5 @@ public class StartScreenManager : LevelManager
     {
         
     }
+
 }
